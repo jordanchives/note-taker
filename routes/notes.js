e8b9694a0a53382174e6672e7848e9e808f30041
Jordan M <jormmiller@gmail.com>
2024-04-22 16:36:00 -0400

    added post and delete routes to notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,42 @@
 const notes = require('express').Router();
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const { readFromFile, writeToFile, readAndAppend } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
 
 // GET Route for retrieving all notes
 notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for a new note
+notes.post('/', (req, res) => {
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuid(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added successfully with ID: ${newNote.note_id}`);
+    } else {
+        res.error('Error adding note');
+    }
+});
+
+// DELETE Route for a specific note
+notes.delete('/:note_id', (req, res) => {
+    const noteId = req.params.note_id;
+    readFromFile('./db/db.json')
+        .then((data) => JSON.parse(data))
+        .then((json) => {
+            const result = json.filter((note) => note.note_id !== noteId);
+
+            writeToFile('./db/db.json', result);
+
+            res.json(`Item ${noteId} deleted`);
+        });
 });
 
 module.exports = notes;
